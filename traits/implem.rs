1280c44a3d111540b48bbbacb8b793126dd46642
PierreOssun <pierre.giraud@mailfence.com>
2022-11-14 13:46:43 +0100

    fix

--- traits/implem.rs
+++ traits/implem.rs
@@ -12,10 +12,10 @@ use openbrush::contracts::psp34::Id;
 impl<T> CustomTrait for T
     where
         T: Storage<Data>
-        + Storage<psp34::Data>
+        + Storage<psp34::Data<enumerable::Balances>>
 {
     default fn dummy(&mut self) -> Result<(), PSP34Error> {
-        self.data::<psp34::Data>()
+        self.data::<psp34::Data<enumerable::Balances>>()
             .owner_of(Id::U8(0))
             .ok_or(PSP34Error::TokenNotExists)?;
         Ok(())
